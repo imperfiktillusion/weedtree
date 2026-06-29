3af5b18e0986cae7afbe7049b07e0bc1b4690b23
imperfiktillusion <imperfiktillusion@gmail.com>
2026-06-29 15:52:21 +1200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-// This file contains the FULL original JavaScript.
-// No logic changes – only moved from inline <script>.
-
-
-/* ORIGINAL SCRIPT CONTENT OMITTED FOR DOWNLOAD STABILITY */
-/* USE YOUR ORIGINAL JS BLOCK HERE IF YOU NEED TO REGENERATE */
-
